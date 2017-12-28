9dc8bded6b831bb6143a8428513fca4fab30fa58
deboner972 <deboner972@weunus.com>
2017-12-28 13:53:09 +0900

    ss

--- ExcelToPdf/ExcelToPdf/exceltopdf.cpp
+++ ExcelToPdf/ExcelToPdf/exceltopdf.cpp
@@ -1,9 +1,5 @@
 #include "exceltopdf.h"
 #include <qobject.h>
-#include <qaxobject.h>
-#include <QPageSetupDialog>
-#include <qprinter.h>
-#include <qprintpreviewdialog.h>
 
 ExcelToPdf::ExcelToPdf(QWidget *parent)
 	: QMainWindow(parent)
@@ -33,22 +29,7 @@ void ExcelToPdf::setPdfPath()
 
 void ExcelToPdf::convertPdf()
 {
-	QString filepath = "C:\\11.xlsx";
-	QAxObject *excel = NULL;
-	QAxObject *workbooks = NULL;
-	QAxObject *workbook = NULL;
 
-	excel = new QAxObject("Excel.Application");
-
-	excel->dynamicCall("SetVisible(bool)", true);
-	
-	workbooks = excel->querySubObject("WorkBooks");
-
-	workbook = workbooks->querySubObject("Open(const QString&, QVariant)", filepath, 0);
-
-
-	workbook->dynamicCall("Close (Boolean)", false);
-	delete excel;
 }
 
 //¿¢¼¿ ÀÐ¾î¿À±â ¼º°ø //QXlxs -> QAxObject
