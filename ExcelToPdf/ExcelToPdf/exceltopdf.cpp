373853beaa8aeebb100d58a7a9dabe2ae11fc278
deboner972 <deboner972@weunus.com>
2017-12-28 13:18:31 +0900

    Third Commit

--- ExcelToPdf/ExcelToPdf/exceltopdf.cpp
+++ ExcelToPdf/ExcelToPdf/exceltopdf.cpp
@@ -1,6 +1,9 @@
 #include "exceltopdf.h"
 #include <qobject.h>
-#include <QtXlsx>
+#include <qaxobject.h>
+#include <QPageSetupDialog>
+#include <qprinter.h>
+#include <qprintpreviewdialog.h>
 
 ExcelToPdf::ExcelToPdf(QWidget *parent)
 	: QMainWindow(parent)
@@ -30,5 +33,24 @@ void ExcelToPdf::setPdfPath()
 
 void ExcelToPdf::convertPdf()
 {
+	QString filepath = "C:\\11.xlsx";
+	QAxObject *excel = NULL;
+	QAxObject *workbooks = NULL;
+	QAxObject *workbook = NULL;
+
+	excel = new QAxObject("Excel.Application");
+
+	excel->dynamicCall("SetVisible(bool)", true);
 	
+	workbooks = excel->querySubObject("WorkBooks");
+
+	workbook = workbooks->querySubObject("Open(const QString&, QVariant)", filepath, 0);
+
+
+	workbook->dynamicCall("Close (Boolean)", false);
+	delete excel;
 }
+
+//엑셀 읽어오기 성공 //QXlxs -> QAxObject
+//엑셀 한 페이지에 모든 열 맞춤 설정 QPageSetupDialog
+//엑셀을 PDF로 저장	//QPrinter
